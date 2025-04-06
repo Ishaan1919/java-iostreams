7770c53a0ca3ba85855f0a998494c0e7ba65f6f5
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-04-06 20:57:04 +0530

    Program for reading and text file and writing it to other file using file input stream and file output stream

--- streams/ReadWriteTextFile.java
+++ streams/ReadWriteTextFile.java
@@ -2,24 +2,46 @@ package streams;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 
 public class ReadWriteTextFile {
     public static void main(String[] args) {
-
-        try(FileInputStream input = new FileInputStream("streams/input.txt");
-            FileOutputStream output = new FileOutputStream("streams/output.txt")){
-
-            int byteData;
-            while((byteData = input.read()) != -1){
-                output.write(byteData);
-            }
-            System.out.println("File copied Successfully");
-        }
-        catch (IOException e){
-            System.out.println("Some Error occured");
+        try(FileInputStream fr = new FileInputStream("streams/input.txt");
+            FileOutputStream fw = new FileOutputStream("streams/output.txt",true)){
+                int data;
+                while((data=fr.read())!=-1){
+                    fw.write(data);
+                }
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }
+
+
+
+
+//package streams;
+//
+//import java.io.FileInputStream;
+//import java.io.FileOutputStream;
+//import java.io.IOException;
+//
+//public class ReadWriteTextFile {
+//    public static void main(String[] args) {
+//
+//        try(FileInputStream input = new FileInputStream("streams/input.txt");
+//            FileOutputStream output = new FileOutputStream("streams/output.txt")){
+//
+//            int byteData;
+//            while((byteData = input.read()) != -1){
+//                output.write(byteData);
+//            }
+//            System.out.println("File copied Successfully");
+//        }
+//        catch (IOException e){
+//            System.out.println("Some Error occured");
+//            e.printStackTrace();
+//        }
+//
+//    }
+//}
