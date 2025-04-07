cbb2580b123f541ebcb2c5e62786c7a88d06a5b7
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-04-07 21:35:13 +0530

    Problem to copy an image file using byte array input and output stream

--- streams/ImageToByteArray.java
+++ streams/ImageToByteArray.java
@@ -4,33 +4,72 @@ import java.io.*;
 
 public class ImageToByteArray {
     public static void main(String[] args) {
+
         String inputImagePath = "C:\\Users\\dell\\Desktop\\bridgelabz-workspace\\java-iostreams\\testImage.png";
         String outputImagePath = "C:\\Users\\dell\\Desktop\\bridgelabz-workspace\\java-iostreams\\testOutputImage.png";
-
-        try (FileInputStream fileInputStream = new FileInputStream(inputImagePath);
-             ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()) {
-
-            // Read file into byte array using while loop
+        try(FileInputStream fis = new FileInputStream(inputImagePath);
+                ByteArrayOutputStream out = new ByteArrayOutputStream()){
             byte[] buffer = new byte[1024];
             int bytesRead;
-            while ((bytesRead = fileInputStream.read(buffer)) != -1) {
-                byteArrayOutputStream.write(buffer, 0, bytesRead);
-            }
-
-            byte[] imageBytes = byteArrayOutputStream.toByteArray();
-
-            // Write byte array to new image file
-            try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(imageBytes);
-                 FileOutputStream fileOutputStream = new FileOutputStream(outputImagePath)) {
-
-                while ((bytesRead = byteArrayInputStream.read(buffer)) != -1) {
-                    fileOutputStream.write(buffer, 0, bytesRead);
+                while((bytesRead=fis.read(buffer))!=-1){
+                    out.write(buffer,0,bytesRead);
                 }
-            }
 
-            System.out.println("Image conversion successful! Check " + outputImagePath);
-        } catch (IOException e) {
-            System.err.println("Error: " + e.getMessage());
+                byte[] image = out.toByteArray();
+
+                try(FileOutputStream fos = new FileOutputStream(outputImagePath);
+                    ByteArrayInputStream in = new ByteArrayInputStream(image)){
+
+                    while((bytesRead=in.read(buffer))!=-1){
+                        fos.write(buffer,0,bytesRead);
+                    }
+
+                    System.out.println("Image copied successfully");
+
+                }
+
+
+        } catch (IOException e){
+            e.printStackTrace();
         }
     }
 }
+
+
+
+//package streams;
+//
+//import java.io.*;
+//
+//public class ImageToByteArray {
+//    public static void main(String[] args) {
+//        String inputImagePath = "C:\\Users\\dell\\Desktop\\bridgelabz-workspace\\java-iostreams\\testImage.png";
+//        String outputImagePath = "C:\\Users\\dell\\Desktop\\bridgelabz-workspace\\java-iostreams\\testOutputImage.png";
+//
+//        try (FileInputStream fileInputStream = new FileInputStream(inputImagePath);
+//             ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()) {
+//
+//            // Read file into byte array using while loop
+//            byte[] buffer = new byte[1024];
+//            int bytesRead;
+//            while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+//                byteArrayOutputStream.write(buffer, 0, bytesRead);
+//            }
+//
+//            byte[] imageBytes = byteArrayOutputStream.toByteArray();
+//
+//            // Write byte array to new image file
+//            try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(imageBytes);
+//                 FileOutputStream fileOutputStream = new FileOutputStream(outputImagePath)) {
+//
+//                while ((bytesRead = byteArrayInputStream.read(buffer)) != -1) {
+//                    fileOutputStream.write(buffer, 0, bytesRead);
+//                }
+//            }
+//
+//            System.out.println("Image conversion successful! Check " + outputImagePath);
+//        } catch (IOException e) {
+//            System.err.println("Error: " + e.getMessage());
+//        }
+//    }
+//}
